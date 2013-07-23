5f7aa02cc7e0495766d476e75eddf636779925ff
S D <simon.r.donnelly@gmail.com>
2013-07-23 06:24:27 +0100

    some constants

--- Schaak/common.h
+++ Schaak/common.h
@@ -1,5 +1,6 @@
 #include <SFML/Graphics/Rect.hpp>
 #include <SFML/Graphics/Color.hpp>
+#include "utilities.h"
 
 static const char* GAME_NAME = "Schaak!";
 
@@ -8,3 +9,12 @@ static const unsigned int WINDOW_MIN_HEIGHT = 768;
 
 static const sf::Color WHITE_SQUARE_COLOR = sf::Color::Color(64,64,64);
 static const sf::Color BLACK_SQUARE_COLOR = sf::Color::Color(128,128,128);
+
+static const int GAME_CONTINUE = 0;
+static const int GAME_VICTORY = 1;
+static const int GAME_DEFEAT = 2;
+
+static const sf::Vector2u BOARD_REGIONS = sf::Vector2u(8,8);
+static const sf::Vector2u REGION_SIZE = sf::Vector2u(100,75);
+
+static const sf::Vector2u BOARD_SIZE = BOARD_REGIONS*BOARD_SIZE;
