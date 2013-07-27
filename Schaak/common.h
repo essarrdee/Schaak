75e0af1ea799defc8f20ef7056fdc03712a2c080
Michał Walczak <mjf.walczak@gmail.com>
2013-07-27 16:03:10 +0200

    ::Color typo

--- Schaak/common.h
+++ Schaak/common.h
@@ -8,8 +8,8 @@ static const std::string GAME_NAME = "Schaak!";
 static const unsigned int WINDOW_MIN_WIDTH = 1024;
 static const unsigned int WINDOW_MIN_HEIGHT = 768;
 
-static const sf::Color WHITE_SQUARE_COLOR = sf::Color::Color(64,64,64);
-static const sf::Color BLACK_SQUARE_COLOR = sf::Color::Color(128,128,128);
+static const sf::Color WHITE_SQUARE_COLOR = sf::Color(64,64,64);
+static const sf::Color BLACK_SQUARE_COLOR = sf::Color(128,128,128);
 
 static const int GAME_CONTINUE = 0;
 static const int GAME_VICTORY = 1;
