94b6a908312927de719277acadc52c52bb2006c6
S D <simon.r.donnelly@gmail.com>
2013-07-27 23:33:53 +0100

    fix addPiece

--- Schaak/PieceManager.cpp
+++ Schaak/PieceManager.cpp
@@ -20,23 +20,22 @@ PieceManager::~PieceManager(void)
 
 PieceID PieceManager::addPiece(Piece* p)
 {
+	PieceID slot;
 	if(freeSlots.size() > 0)
 	{
 		PieceID slot = freeSlots.front();
 		freeSlots.pop_front();
 		pieces[slot] = *p;
-		pieces[slot].id = slot;
-		pieces[slot].uniqueID = nextUniqueID;
-		return slot;
 	}
 	else
 	{
 		PieceID slot = pieces.size();
 		pieces.push_back(*p);
-		pieces[slot].id = slot;
-		pieces[slot].uniqueID = nextUniqueID;
-		return slot;
 	}
+	pieces[slot].id = slot;
+	pieces[slot].uniqueID = nextUniqueID;
+	nextUniqueID++;
+	return slot;
 }
 
 void PieceManager::freeSlot(PieceID slot)
