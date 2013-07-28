ce8ca15f3e55593adb5b49bb2f0ddc9fd9a46be1
S D <simon.r.donnelly@gmail.com>
2013-07-28 13:47:30 +0100

    seed the RNG

--- Schaak/main.cpp
+++ Schaak/main.cpp
@@ -1,10 +1,14 @@
 #include "Logger.h"
 #include "Interface.h"
+#include <time.h>
+#include <random>
 
 int main(int argc, char** argv)
 {
 	Logger::init("Schaak.log");
 	LINFO("Main started");
+	srand(time(0));
+	LINFO("RNG seeded");
 	Interface* Schaak = new Interface();
 	return 0;
 
